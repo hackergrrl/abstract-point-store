2c59ce2f3c5fb86a5ec96d6bce3a64ae3c24337c
Stephen Whitmore <sww@eight45.net>
2018-01-30 14:20:43 -0800

    test: various types

--- test/index.js
+++ test/index.js
@@ -67,5 +67,48 @@ module.exports = function (test, Store, backend) {
       })
     }
   })
+
+  test('various types', function (t) {
+    var n = 200
+    var kdb = Store({
+      types: [ 'f64', 'f32', 'uint32' ],
+      store: backend()
+    })
+    var data = []
+    var pending = n
+    for (var i = 0; i < n; i++) (function () {
+      var x = Math.random() * 200 - 100
+      var y = Math.random() * 200 - 100
+      var loc = Math.floor(Math.random() * 1000)
+      data.push({ point: [x,y], value: loc })
+      kdb.insert([x,y], loc, function (err) {
+        t.ifError(err, 'insert ifError')
+        kdb.query([x,y], function (err, pts) {
+          t.ifError(err, 'query ifError')
+          t.equal(pts.length, 1, 'single query result for single point')
+          if (pts[0]) {
+            approx(t, pts[0].point, [x,y])
+            t.equal(pts[0].value, loc, 'point value')
+          } else t.fail('no point')
+          if (--pending === 0) check()
+        })
+      })
+    })()
+
+    function check () {
+      kdb.query([[15,50],[-60,10]], function (err, pts) {
+        t.ifError(err)
+        var expected = data.filter(function (p) {
+          var pt = p.point
+          return pt[0] >= 15 && pt[0] <= 50
+            && pt[1] >= -60 && pt[1] <= 10
+        })
+        for (var i = 0; i < Math.max(pts.length, expected.length); i++) {
+          approx(t, pts[i], expected[i])
+        }
+        t.end()
+      })
+    }
+  })
 }
 
