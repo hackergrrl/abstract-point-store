73a43ab0daf5a9ed5bc3a7a04d84b0f90495e6e3
Stephen Whitmore <sww@eight45.net>
2018-01-30 14:22:29 -0800

    test: big

--- test/index.js
+++ test/index.js
@@ -12,13 +12,13 @@ function approx (t, a, b) {
 module.exports = function (test, Store, backend) {
   test('one point', function (t) {
     t.plan(3)
-    var kdb = Store({
+    var geo = Store({
       types: [ 'float32', 'float32', 'uint32' ],
       store: backend()
     })
-    kdb.insert([1,2], 9999, function (err) {
+    geo.insert([1,2], 9999, function (err) {
       t.ifError(err)
-      kdb.query([1,2], function (err, pts) {
+      geo.query([1,2], function (err, pts) {
         t.ifError(err)
         t.deepEqual(pts, [ { point: [1,2], value: 9999 } ])
       })
@@ -27,7 +27,7 @@ module.exports = function (test, Store, backend) {
 
   test('many points', function (t) {
     var n = 20
-    var kdb = Store({
+    var geo = Store({
       types: [ 'float32', 'float32', 'uint32' ],
       store: backend()
     })
@@ -38,9 +38,9 @@ module.exports = function (test, Store, backend) {
       var y = Math.random() * 200 - 100
       var loc = Math.floor(Math.random() * 1000)
       data.push({ point: [x,y], value: loc })
-      kdb.insert([x,y], loc, function (err) {
+      geo.insert([x,y], loc, function (err) {
         t.ifError(err, 'insert ifError')
-        kdb.query([x,y], function (err, pts) {
+        geo.query([x,y], function (err, pts) {
           t.ifError(err, 'query ifError')
           t.equal(pts.length, 1, 'single query result for single point')
           if (pts[0]) {
@@ -53,7 +53,7 @@ module.exports = function (test, Store, backend) {
     })()
 
     function check () {
-      kdb.query([[15,50],[-60,10]], function (err, pts) {
+      geo.query([[15,50],[-60,10]], function (err, pts) {
         t.ifError(err)
         var expected = data.filter(function (p) {
           var pt = p.point
@@ -70,7 +70,7 @@ module.exports = function (test, Store, backend) {
 
   test('various types', function (t) {
     var n = 200
-    var kdb = Store({
+    var geo = Store({
       types: [ 'f64', 'f32', 'uint32' ],
       store: backend()
     })
@@ -81,9 +81,9 @@ module.exports = function (test, Store, backend) {
       var y = Math.random() * 200 - 100
       var loc = Math.floor(Math.random() * 1000)
       data.push({ point: [x,y], value: loc })
-      kdb.insert([x,y], loc, function (err) {
+      geo.insert([x,y], loc, function (err) {
         t.ifError(err, 'insert ifError')
-        kdb.query([x,y], function (err, pts) {
+        geo.query([x,y], function (err, pts) {
           t.ifError(err, 'query ifError')
           t.equal(pts.length, 1, 'single query result for single point')
           if (pts[0]) {
@@ -96,7 +96,50 @@ module.exports = function (test, Store, backend) {
     })()
 
     function check () {
-      kdb.query([[15,50],[-60,10]], function (err, pts) {
+      geo.query([[15,50],[-60,10]], function (err, pts) {
+        t.ifError(err)
+        var expected = data.filter(function (p) {
+          var pt = p.point
+          return pt[0] >= 15 && pt[0] <= 50
+            && pt[1] >= -60 && pt[1] <= 10
+        })
+        for (var i = 0; i < Math.max(pts.length, expected.length); i++) {
+          approx(t, pts[i], expected[i])
+        }
+        t.end()
+      })
+    }
+  })
+
+  test('big', function (t) {
+    var n = 5000
+    var geo = Store({
+      types: [ 'float32', 'float32', 'uint32' ],
+      store: backend()
+    })
+    var data = []
+    var pending = n
+    for (var i = 0; i < n; i++) (function () {
+      var x = Math.random() * 200 - 100
+      var y = Math.random() * 200 - 100
+      var loc = Math.floor(Math.random() * 1000)
+      data.push({ point: [x,y], value: loc })
+      geo.insert([x,y], loc, function (err) {
+        t.ifError(err, 'insert ifError')
+        geo.query([x,y], function (err, pts) {
+          t.ifError(err, 'query ifError')
+          t.equal(pts.length, 1, 'single query result for single point')
+          if (pts[0]) {
+            approx(t, pts[0].point, [x,y])
+            t.equal(pts[0].value, loc, 'point value')
+          } else t.fail('no point')
+          if (--pending === 0) check()
+        })
+      })
+    })()
+
+    function check () {
+      geo.query([[15,50],[-60,10]], function (err, pts) {
         t.ifError(err)
         var expected = data.filter(function (p) {
           var pt = p.point
